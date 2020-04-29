c2ce3c99175b7e13c253b49d7240af75c6189c85
Thomas Simonson <thomas.simonson@gmail.com>
2020-04-29 14:30:20 +0200

    Version caching the inverse matrix in the 'myMatrixObject' environment
    Row 17, the deep assignment arrow <<- is replaced by a regular assignment arrow <-. This moves the 'j' variable from the global environment to the 'myMatrixObject' environment

--- cachematrix.R
+++ cachematrix.R
@@ -4,73 +4,67 @@
 # The function 'MakeCacheMatrix' is created in the global environment, it contains the template for a storage environment with...
 # 1) Two data objects: 'x' (to recieve a matrix) and 'j' (to store its inverse matrix)
 # 2) a list of functions (with their own environment) that point to those two objects (in their parent environment)
-# At this point, all this is just an empty template that the function will copy every time it is run with a matrix as argument.
+# At this point, all this is just an empty template that the function will copy every time it is run with a matrix argument.
 
 # A SPECIFIC STORAGE ENVIRONMENT FOR OUR MATRIX : THE FUNCTION 'MakeCacheMatrix' IS RUN
 #--------------------------------------------------------------------------------------
-# When 'MakeCacheMatrix' is run, with a matrix as argument, it creates a complete copy of its template environment for that specific matrix.
+# When 'MakeCacheMatrix' is run, with a matrix argument, it creates a complete copy of its template environment for that specific matrix.
 # The output is a list of functions pointing to the places in that new environment where the matrix is stored ('x')
 # and where the inverse matrix will be cached ('j').
 # That output can be assigned to a variable name in the global environment (e.g. 'MyMatrixObject') and later fed to the 'cacheSolve' function.
 
-makeCacheMatrix <- function(x = matrix()) { # The function takes a matrix as argument. The default value is an empty 1x1 matrix.
-        j<<-NULL # 'j' is set to 'NULL'. The deep assignment arrow(<<-) creates a variable in the parent environment of 'makeCacheMatrix' (i.e. the global environment).
-        # A regular assignment arrow (<-) would work to store and recall the inverse matrix from within the function,
-        # but the variable 'j' would be located in the current environment of 'makeCacheMatrix' and would not be directly accessible to us in the global environment.
+makeCacheMatrix <- function(x = matrix()) { # The function takes a matrix argument. The default value is an empty 1x1 matrix.
+        j<-NULL # 'j' is set to 'NULL'. The regular assignment arrow(<-) creates a variable in the current environment of 'makeCacheMatrix'.
         
         # - 'set' recycles our storage place for another matrix.
         # It allows to change matrices and to reset the inverse matrix to 'NULL' without having to use 'MakeCacheMatrix' again.
-        # The deep assignment arrow (<<-) modifies the variables 'x' in the parent 'MakeCacheMatrix' environment and 'j' found in the grand-parent global environment.
+        # The deep assignment arrows (<<-) modify the variables 'x' and 'j' in the parent environment (i.e. the 'MakeCacheMatrix' environment).
         # The data previously cached in the variables 'x' and 'j' is lost.
-        # To cache the inverse of several matrices, we would need to run 'MakeCacheMatrix' for each one of them.
+        # To cache the inverse of several matrices, we would need to run 'MakeCacheMatrix' for each one of them separately.
         set<-function(y){
                 j<<-NULL
                 x<<-y
         }
         
-        
-        # - 'get' allows to retrieve the matrix 'x'. The variable 'x' is absent from its environment, but due to lexical scoping,
-        #  the function is able to find it in its parent 'MyMatrixObject' environment.
+                # - 'get' allows to retrieve the matrix 'x'. The variable 'x' is absent from its environment, but due to lexical scoping,
+        #  the function is able to find it in its parent environment (i.e. the 'MyMatrixObject' environment).
         get<-function(){
                 x
         }
         
         # - 'setinv' allows to cache the inverse of the matrix, after the function 'cacheSolve' has calculated it.
-        # 'j' is absent from the 'setinv' environment, but is found due to lexical scoping in the grand-parent global environment,
-        # and modified using the deep assignment arrow (<<-).
+        # Again, thanks to lexical scoping, 'setinv' is able to find 'j' in its parent environment (i.e. the 'MyMatrixObject' environment),
+        # and modify it using the deep assignment arrow (<<-).
         setinv<-function(i){
                 j<<-i
         }
         
-        # - 'getinv' retrieves the cached inverse matrix.
-        # The variable 'j' is absent from its environment, but the functions walks up parent environments 
-        # until it finds it in the grand-parent global environment.
+        # - 'getinv' retrieves the cached inverse matrix in exactly the same way as the 'get' function.
         getinv<-function(){
                 j
         }
         
-        # 'MakeCacheMatrix'returns a list in which each element (function) is named, allowing us to call individual functions from 'cacheSolve'
+        # 'makeCacheMatrix'returns a list in which each element (each function) is named, allowing us to call individual functions from 'cacheSolve'
         list(set = set,
              get = get,
              setinv = setinv,
              getinv = getinv)
 }
 
-
-# 'cacheSolve' takes as argument the 'MyMatrixObject' list of functions created in the global environment by the function 'makeCacheMatrix'.
-# It calls the functions contained in that list, which in turn look for the necessary variables following the rules of lexical scoping.
-# (i.e. their own environment -> the parent 'MyMatrixObject' environment -> the grand-parent global environment).
+# ACCESSING THE STORAGE ENVIROMENT OF THE MATRIX AND COMPUTING THE INVERSE MATRIX : the 'cacheSolve' function
+# -----------------------------------------------------------------------------------------------------------
+# The argument of 'cacheSolve' is the list of functions created by the 'makeCacheMatrix' function in its specific 'myMatrixObject' environment.
+# 'cacheSolve' calls the functions contained in that list, which in turn look for the necessary variables following the rules of lexical scoping.
 
 # If an inverse matrix exists but has not been calculated yet, it is computed, stored and returned,
 # otherwise, the cached inverse matrix already computed is retrieved.
 
-# The argument of 'cacheSolve' is the list of functions created by the 'makeCacheMatrix' function in the global environment.
-# In the code below, that argument does not need to be called 'x'. It is a local variable in the current environment of 'cacheSolve',
+# In the code below, the argument of 'cacheSolve' does not need to be called 'x'. It is a local variable in the current environment of 'cacheSolve'.
 # I find it clearer to call it 'mmo' for 'my matrix object'.
 
 cacheSolve <- function(mmo, ...) { 
         
-        # The content of the 'j' variable is retrieved by the 'mmo$getinv' function from its grand-parent global environment.
+        # The content of the 'j' variable is retrieved by the 'mmo$getinv' function from its parent 'myMatrixObject' environment.
         # and then assigned (<-) to a variable 'i' in the current environment of the 'cacheSolve' function.
         i<-mmo$getinv()
         
@@ -81,7 +75,7 @@ cacheSolve <- function(mmo, ...) {
         }
         
         # If the inverse matrix hasn't been computed yet, 
-        # the matrix is retrieved, running the function 'mmo$get' from the 'MyMatrixObject' list.
+        # the matrix is retrieved, running the function 'mmo$get' from the 'myMatrixObject' list of functions.
         # The matrix is located in the parent environment of the 'mmo$get' function (i.e. the 'MyMatrixObject' environment).
         x<-mmo$get()
         
@@ -94,7 +88,7 @@ cacheSolve <- function(mmo, ...) {
         # A square matrix has an inverse iff its determinant is not 0.
         if(det(x)!=0){   # After checking this,...
                 i<-solve(x)   # the inverse of the matrix is computed and assigned to a variable 'i' in the current environment of 'cacheSolve'.
-                mmo$setinv(i) # We call the 'setinv' function from the 'MyMatrixObject' list to cache the result in the its grand-parent global environment.
+                mmo$setinv(i) # We call the 'setinv' function from the 'MyMatrixObject' list to cache the result in the parent 'myMatrixObject' environment.
                 return(i)     # The result is returned.
         }
         
